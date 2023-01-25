ed28c8c306b063d347cb146b32e3552ac06d73af
norabbito <60084944+norabbito@users.noreply.github.com>
2023-01-25 10:19:42 +0900

    Cleanup code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const mapping = {
-  0: "rock",
-  1: "paper",
-  2: "scissors"
+  0: 'rock',
+  1: 'paper',
+  2: 'scissors',
 };
 
 function getComputerChoice() {
@@ -9,28 +9,28 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let choice = prompt("Choose a weapon (Rock, Paper, Scissors)");
+  const choice = prompt('Choose a weapon (Rock, Paper, Scissors)', '');
 
   switch (choice.toLowerCase()) {
-    case "rock": return 0;
-    case "paper": return 1;
-    case "scissors": return 2;
-    default: alert("Unknown choice");
+    case 'rock': return 0;
+    case 'paper': return 1;
+    case 'scissors': return 2;
+    default: return undefined;
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if ((playerSelection + 1) % 3 == computerSelection) {
-    return "Computer wins!";
-  } else if (playerSelection == computerSelection) {
-    return "It's a tie!";
-  } else {
-    return "Player wins!";
+  if ((playerSelection + 1) % 3 === computerSelection) {
+    return 'Computer wins!';
   }
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  }
+  return 'Player wins!';
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i += 1) {
     const computerSelection = getComputerChoice();
     const playerSelection = getPlayerChoice();
     if (playerSelection == null) break;
